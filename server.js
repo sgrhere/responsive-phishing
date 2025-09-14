027aa3af48bf3478c65dfaadb6506bb6eb50b7dd
Sagar <152954850+sgrhere@users.noreply.github.com>
2025-09-14 17:32:29 +0545

    Update server.js

--- server.js
+++ server.js
@@ -8,7 +8,6 @@ const bodyParser = require('body-parser')
 app.use(bodyParser.json());  // doing this, it will store converted data into req.body 
 const PORT = process.env.PORT || 3000;
 
-
 // Serve static files from 'public' folder
 app.use(express.static('public'));
 
@@ -16,14 +15,11 @@ app.get('/', function(req, res) {
     res.send('Hello World! Welcome to the plastic server...')
 })
 
-
 //import routers
 const dataRoutes = require('./routes/personRoutes')
 
 //use routers
 app.use('/submit',dataRoutes)
-
-
 
 app.listen(PORT, () => {
     console.log("Server is live now");
